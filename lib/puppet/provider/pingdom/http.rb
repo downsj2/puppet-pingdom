732fafc1376bf725eb309fe9e22e828df1b189e5
Cliff Wells <cwells@focusvision.com>
2017-11-03 01:44:24 -0700

    set all properties

--- lib/puppet/provider/pingdom/http.rb
+++ lib/puppet/provider/pingdom/http.rb
@@ -24,15 +24,28 @@ Puppet::Type.type(:pingdom).provide(:http) do
     end
 
     def create
+        params = {
+            :type => 'http',
+            :name => @resource[:name],
+            :host => @resource[:host],
+            :url  => @resource[:url],
+            :paused                   => @resource[:paused],
+            :resolution               => @resource[:resolution],
+            :sendnotificationwhendown => @resource[:sendnotificationwhendown],
+            :notifywhenbackup         => @resource[:notifywhenbackup],
+            :ipv6                     => @resource[:ipv6],
+            :notifyagainevery         => @resource[:notifyagainevery],
+            :responsetime_threshold   => @resource[:responsetime_threshold],
+            # :userids                  => @resource[:userids],
+            # :probe_filters            => @resource[:probe_filters],
+            # :integrationids           => @resource[:integrationids],
+            # :teamids                  => @resource[:teamids],
+            # :tags                     => @resource[:tags]
+        }
         if check = client.find_check(@resource[:name])
-            client.modify_check check, @resource
+            client.modify_check check, params
         else
-            client.create_check @resource[:name], {
-                :type => 'http',
-                :name => @resource[:name],
-                :host => @resource[:host],
-                :url  => @resource[:url]
-            }
+            client.create_check @resource[:name], params
         end
     end
 
