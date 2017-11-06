07ebe383d745a0f46ea21d3a931d939ecd4424ce
Cliff Wells <cwells@focusvision.com>
2017-11-06 09:25:49 -0800

    remove really long property from example

--- tests/init.pp
+++ tests/init.pp
@@ -4,18 +4,17 @@ Pingdom_check {
     appkey   => $pingdom_appkey
 }
 
-pingdom_check { "http://${facts['fqdn']}/check":
-    ensure                   => present,
-    provider                 => 'http',
-    host                     => $facts['fqdn'],
-    url                      => '/check',
-    paused                   => true,
-    resolution               => 5,
-    ipv6                     => false,
-    sendnotificationwhendown => 2,
-    notifyagainevery         => 0,
-    notifywhenbackup         => false
-    tags                     => ['test', 'web']
+pingdom_check { "httpts['fqdn']}/check":
+    ensure           => present,
+    provider         => 'http',
+    host             => $facts['fqdn'],
+    url              => '/check',
+    paused           => true,
+    resolution       => 5,
+    ipv6             => false,
+    notifyagainevery => 0,
+    notifywhenbackup => false
+    tags             => ['test', 'web']
 }
 
 pingdom_check { "dns://${facts['fqdn']}":
