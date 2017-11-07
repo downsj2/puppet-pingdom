25c7045510a4eb3c73c678e192829fdce6a0197c
Cliff Wells <cwells@focusvision.com>
2017-11-07 15:38:09 -0800

    pop3 working

--- lib/puppet/provider/pingdom_check/pop3.rb
+++ lib/puppet/provider/pingdom_check/pop3.rb
@@ -1,4 +1,33 @@
 Puppet::Type.type(:pingdom_check).provide(:pop3, :parent => :check_base) do
-    has_features :port, :stringtoexpect, :encryption
+    has_features :host, :port, :stringtoexpect, :encryption
+
+    def encryption
+        begin
+            @check['type']['pop3']['encryption']
+        rescue => exception
+            :absent
+        end
+    end
+
+    def host
+        @check.fetch('hostname', :absent)
+    end
+
+    def port
+        begin
+            @check['type']['pop3']['port']
+        rescue => exception
+            :absent
+        end
+    end
+
+    def stringtoexpect
+        begin
+            @check['type']['pop3']['stringtoexpect']
+        rescue => exception
+            :absent
+        end
+    end
+
     accessorize
 end
