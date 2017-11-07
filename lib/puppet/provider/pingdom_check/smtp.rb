c31940d6638032742842a611948fb4c507d9c1c2
Cliff Wells <cwells@focusvision.com>
2017-11-07 15:40:25 -0800

    smtp working

--- lib/puppet/provider/pingdom_check/smtp.rb
+++ lib/puppet/provider/pingdom_check/smtp.rb
@@ -1,4 +1,33 @@
 Puppet::Type.type(:pingdom_check).provide(:smtp, :parent => :check_base) do
-    has_features :port, :auth, :stringtoexpect, :encryption
+    has_features :host, :port, :auth, :stringtoexpect, :encryption
+
+    def encryption
+        begin
+            @check['type']['smtp']['encryption']
+        rescue => exception
+            :absent
+        end
+    end
+
+    def host
+        @check.fetch('hostname', :absent)
+    end
+
+    def port
+        begin
+            @check['type']['smtp']['port']
+        rescue => exception
+            :absent
+        end
+    end
+
+    def stringtoexpect
+        begin
+            @check['type']['smtp']['stringtoexpect']
+        rescue => exception
+            :absent
+        end
+    end
+
     accessorize
 end
