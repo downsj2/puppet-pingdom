21eed31b4cf45b0de983c613495ec0e0127b05e8
Cliff Wells <cwells@focusvision.com>
2017-11-07 15:45:06 -0800

    tcp and udp working

--- lib/puppet/provider/pingdom_check/tcp.rb
+++ lib/puppet/provider/pingdom_check/tcp.rb
@@ -1,4 +1,33 @@
 Puppet::Type.type(:pingdom_check).provide(:tcp, :parent => :check_base) do
-    has_features :port, :stringtosend, :stringtoexpect
+    has_features :host, :port, :stringtosend, :stringtoexpect
+
+    def host
+        @check.fetch('hostname', :absent)
+    end
+
+    def port
+        begin
+            @check['type']['tcp']['port']
+        rescue => exception
+            :absent
+        end
+    end
+
+    def stringtoexpect
+        begin
+            @check['type']['tcp']['stringtoexpect']
+        rescue => exception
+            :absent
+        end
+    end
+
+    def stringtosend
+        begin
+            @check['type']['tcp']['stringtosend']
+        rescue => exception
+            :absent
+        end
+    end
+
     accessorize
 end
--- lib/puppet/provider/pingdom_check/udp.rb
+++ lib/puppet/provider/pingdom_check/udp.rb
@@ -1,4 +1,33 @@
 Puppet::Type.type(:pingdom_check).provide(:udp, :parent => :check_base) do
-    has_features :port, :stringtosend, :stringtoexpect
+    has_features :host, :port, :stringtosend, :stringtoexpect
+
+    def host
+        @check.fetch('hostname', :absent)
+    end
+
+    def port
+        begin
+            @check['type']['udp']['port']
+        rescue => exception
+            :absent
+        end
+    end
+
+    def stringtoexpect
+        begin
+            @check['type']['udp']['stringtoexpect']
+        rescue => exception
+            :absent
+        end
+    end
+
+    def stringtosend
+        begin
+            @check['type']['udp']['stringtosend']
+        rescue => exception
+            :absent
+        end
+    end
+
     accessorize
 end
