1a283e11be702b3a436bef33b668aac547fe653e
Cliff Wells <cwells@focusvision.com>
2017-11-15 15:27:28 -0800

    catch missing params

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,16 @@
 class pingdom {
-  package { 'faraday':
+  ensure_packages(['faraday'], {
     ensure   => present,
     provider => 'puppet_gem'
-  }
+  })
 
   $definitions = lookup('pingdom', Hash)
 
   $defaults = {
-    'account_email' => $definitions['account_email'],
-    'user_email'    => $definitions['user_email'],
-    'password'      => $definitions['password'],
-    'appkey'        => $definitions['appkey'],
+    'account_email' => pick($definitions['account_email'], 'Missing parameter `account_email`'),
+    'user_email'    => pick($definitions['user_email'], 'Missing parameter `user_email`'),
+    'password'      => pick($definitions['password'], 'Missing parameter `password`'),
+    'appkey'        => pick($definitions['appkey'], 'Missing parameter `appkey`'),
     'require'       => Package['faraday']
   }
 
