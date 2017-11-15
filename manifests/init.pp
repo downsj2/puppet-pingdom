13238aa6fa2f8af4fdc43a54f861bcb5d2776f84
Cliff Wells <cwells@focusvision.com>
2017-11-15 14:09:45 -0800

    install faraday gem

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,17 @@
 class pingdom {
+  package { 'faraday':
+    ensure   => present,
+    provider => 'pe_gem'
+  }
+
   $definitions = lookup('pingdom', Hash)
 
   $defaults = {
     'account_email' => $definitions['account_email'],
     'user_email'    => $definitions['user_email'],
     'password'      => $definitions['password'],
-    'appkey'        => $definitions['appkey']
+    'appkey'        => $definitions['appkey'],
+    'require'       => Package['faraday']
   }
 
   create_resources('pingdom_user', $definitions['users'], $defaults)
